70d522e6dffc7ad30232174d5c8033297f263284
cornelius rizki pratama <117552481+CorneliusRizky@users.noreply.github.com>
2025-10-21 11:31:09 +0700

    struktur data tugas 1.java

--- struktur data tugas 1.java
+++ struktur data tugas 1.java
@@ -1,8 +1,6 @@
-
 import java.util.LinkedList;
 
 int StrukturBaris = 0;
-
 
 String KataBaru = "Deklarasi tipe data String";
 
@@ -14,7 +12,6 @@ String[][] Angka = {
     {"1", "3", "5"},
     {"14", "19", "20"},
     {"22", "27", "29"}
-
 };
 
 
@@ -24,6 +21,15 @@ listAngka.add(19);
 listAngka.add(44);
 listAngka.add(60);
 listAngka.add(72);
+
+
+int[] empatAngka = {7, 10, 20, 23};
+
+String[][] Angka = {
+    {"1", "3", "5"},
+    {"14", "19", "20"},
+    {"22", "27", "29"}
+};
 
 
 
